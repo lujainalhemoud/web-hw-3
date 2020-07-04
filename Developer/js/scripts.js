5759e1d94ef09dbc83ff5d5bce8a6d63e3e8833b
lujainalhemoud <66439694+lujainalhemoud@users.noreply.github.com>
2020-07-05 01:06:54 +0300

    doo

--- Developer/js/scripts.js
+++ Developer/js/scripts.js
@@ -1,44 +1,59 @@
-const language = ["html", "css", "JavaScript", "php","swift","java"];
+const language = ["html", "css", "JavaScript", "php", "swift", "java"];
 const needed_language = 3;
 
-console.log(" مرحبا بك في برنامج توظيف المبرمجون")
+console.log(" مرحبا بك في برنامج توظيف المبرمجون");
 
+let name = promot("ما اسمك");
+let age = parseInt(prompt("كم عمرك "));
+let experience = parseInt(prompt("كم عدد سنوات الخبرة"));
 
-let name = promot("ما اسمك")
-let age = parseInt (prompt("كم عمرك "))
-let experience = parseInt (prompt("كم عدد سنوات الخبرة"))
+console.log("لغات البرمجة:");
 
-console.log("لغات البرمجة:")
-
-for ( let i = 1; i <= languages.length; i++) {
-    console.log( i + " " + languages[ i-1 ]);
+for (let i = 1; i <= languages.length; i++) {
+  console.log(i + " " + languages[i - 1]);
 }
 
-let skill1 = prompt("اختر اللغة")
-let skill2 = prompt ("اختر لغة اخرى")
+let skill1 = prompt("اختر اللغة");
+let skill2 = prompt("اختر لغة اخرى");
 
-  if (
-age> 25 && age <44 &&
-experience > 3&&
-( skill1 = needed_language || skill2 = needed_language)
-  ) {
-      console.log("مقبول");
-  } else {
-      console.log("مرفوض");
-  }
+if (
+  age > 25 &&
+  age < 44 &&
+  experience > 3 &&
+  (skill1 = needed_language || skill2 = needed_language)
+) {
+  console.log("مقبول");
+} else {
+  console.log("مرفوض");
+}
 
 function looger(temperatures) {
-for ( let i = 0; i < temperatures.length; i++){
+  for (let i = 0; i < temperatures.length; i++) {
     console.log(temperatures[i]);
-}
-}
-
-function toCel( temperatures){
-    for ( let i = 0; i < temperatures.length; i++) {
-     result_array.push(parseInt((temperatures[i]-32)*(5 / 9)));
-
-    }
-return result_array;
+  }
 }
 
-function
+function toCel(temperatures) {
+  for (let i = 0; i < temperatures.length; i++) {
+    result_array.push(parseInt((temperatures[i] - 32) * (5 / 9)));
+  }
+  return result_array;
+}
+
+function hottestDays(temperatures, min_value) {
+  for (let i = 0; i < temperatures.length; i++) {
+    hot.push(temperatures[i]);
+  }
+  return hot;
+}
+
+let feh_temp_array = [32, 70, 99, 106];
+let threshold = 70;
+let hot = [];
+let result_array = [];
+
+function logHottestDays(input_array, min_value) {
+  logger(tocel(hottestDays(input_array, min_value)));
+}
+
+logHottestDays(feh_temp_arry, threshold);
